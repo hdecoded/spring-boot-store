495469473c2a0d1df8c9bbbeb7686a490bd5c4f4
hdecoded <hdecoded@yahoo.com>
2025-03-05 06:37:15 +0530

    Added Mysql  and spring data JPA dependencies

--- src/main/java/com/hdecoded/store/StoreApplication.java
+++ src/main/java/com/hdecoded/store/StoreApplication.java
@@ -1,6 +1,5 @@
 package com.hdecoded.store;
 
-import com.hdecoded.store.user.User;
 import com.hdecoded.store.user.UserService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,17 +9,7 @@ import org.springframework.context.ApplicationContext;
 public class StoreApplication {
 
     public static void main(String[] args) {
-//        ApplicationContext context = SpringApplication.run(StoreApplication.class);
-//        ConfigurableApplicationContext context = SpringApplication.run(StoreApplication.class);
-//        var hv = context.getBean(HeavyResource.class);
-//        var orderService = context.getBean(OrderService.class);
-//        var orderService2 = context.getBean(OrderService.class);
-//        orderService.placeOrder();
-//        context.close();
         ApplicationContext context = SpringApplication.run(StoreApplication.class, args);
         var userService = context.getBean(UserService.class);
-        userService.registerUser(new User(1L, "hd@outlook.in", "Pass", "Hemanth"));
-        userService.registerUser(new User(1L, "hd@outlook.in", "Pass", "Hemanth"));
-        userService.registerUser(new User(2L, "sp@outlook.in", "Pass", "Sowmya"));
     }
 }
